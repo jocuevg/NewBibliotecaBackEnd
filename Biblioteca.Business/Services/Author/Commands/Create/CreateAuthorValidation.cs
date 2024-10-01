0b76ae937dcc289dd4c46c6ef7397ee35b6f4f05
jcuevasg <jcuevasg@hiberus.com>
2024-10-01 11:15:13 +0200

    mensaje validation autor

--- Biblioteca.Business/Services/Author/Commands/Create/CreateAuthorValidation.cs
+++ Biblioteca.Business/Services/Author/Commands/Create/CreateAuthorValidation.cs
@@ -9,8 +9,8 @@ namespace Biblioteca.Business.Services.Author.Commands.Create;
 public class CreateAuthorValidation : AbstractValidator<CreateAuthor>
 {
     public CreateAuthorValidation() {
-        RuleFor(x => x.Nombre).MaximumLength(100).WithMessage("Maximo 100 caracteres"); ;
-        RuleFor(x => x.Apellidos).MaximumLength(200).WithMessage("Maximo 200 caracteres"); ;
+        RuleFor(x => x.Nombre).MaximumLength(100).WithMessage("Maximo nombre de 100 caracteres"); 
+        RuleFor(x => x.Apellidos).MaximumLength(200).WithMessage("Maximo apellidos de 200 caracteres"); 
         RuleFor(x => x.Edad).GreaterThan(0).WithMessage("Edad debe ser positiva");
     }
 }
--- Biblioteca.Business/Services/Author/Commands/Update/UpdateAuthorValidation.cs
+++ Biblioteca.Business/Services/Author/Commands/Update/UpdateAuthorValidation.cs
@@ -5,8 +5,8 @@ public class UpdateAuthorValidation : AbstractValidator<UpdateAuthor>
 {
     public UpdateAuthorValidation()
     {
-        RuleFor(x => x.Nombre).MaximumLength(100).WithMessage("Maximo nombre de 100 caracteres"); ;
-        RuleFor(x => x.Apellidos).MaximumLength(200).WithMessage("Maximo apellidos de 200 caracteres"); ;
+        RuleFor(x => x.Nombre).MaximumLength(100).WithMessage("Maximo nombre de 100 caracteres"); 
+        RuleFor(x => x.Apellidos).MaximumLength(200).WithMessage("Maximo apellidos de 200 caracteres"); 
         RuleFor(x => x.Edad).GreaterThan(0).WithMessage("Edad debe ser positiva");
     }
 }
